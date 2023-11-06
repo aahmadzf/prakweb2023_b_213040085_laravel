db26b81d13134cb5058092a89d1d1a3433da4f2a
aahmadzf <aahmadzf0@gmail.com>
2023-11-06 18:03:21 +0700

    Factory & Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,24 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(3)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            'name' => 'Ahmad Zidane',
-            'email' => 'ahmad@gmail.com',
-            'password' => bcrypt('admin'),
-        ]);
+        // User::create([
+        //     'name' => 'Ahmad Zidane',
+        //     'email' => 'ahmad@gmail.com',
+        //     'password' => bcrypt('admin'),
+        // ]);
 
-        User::create([
-            'name' => 'Faturrahman',
-            'email' => 'fatur2@gmail.com',
-            'password' => bcrypt('admin'),
-        ]);
+        // User::create([
+        //     'name' => 'Faturrahman',
+        //     'email' => 'fatur2@gmail.com',
+        //     'password' => bcrypt('admin'),
+        // ]);
 
         Category::create([
             'name' => 'Web Programming',
@@ -45,43 +45,45 @@ class DatabaseSeeder extends Seeder
             'slug' => 'Personal',
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus ',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?',
-            'category_id' => 1,
-            'user_id' => 1,
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus ',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
 
-        ]);
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus ',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?',
-            'category_id' => 1,
-            'user_id' => 2,
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus ',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?',
+        //     'category_id' => 1,
+        //     'user_id' => 2,
 
-        ]);
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus ',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?',
-            'category_id' => 2,
-            'user_id' => 1,
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus ',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?',
+        //     'category_id' => 2,
+        //     'user_id' => 1,
 
-        ]);
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Keempat',
-            'slug' => 'judul-keempat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus ',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?',
-            'category_id' => 2,
-            'user_id' => 2,
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus ',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reiciendis modi tenetur debitis nam qui id dignissimos cupiditate, saepe magni accusamus repellendus odio accusantium provident autem quis repellat ducimus, quod eum. Officia labore soluta molestiae maiores consectetur? Sunt, obcaecati. Molestiae explicabo vitae nemo non nisi asperiores earum doloribus officiis obcaecati?',
+        //     'category_id' => 2,
+        //     'user_id' => 2,
+        // ]);
+
+        Post::factory(20)->create();
     }
 }
