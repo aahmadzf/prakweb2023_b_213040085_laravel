c94ad01ec2cddea81fd31d96d1810d71b810a565
aahmadzf <aahmadzf0@gmail.com>
2023-11-26 19:42:54 +0700

    Update & Delete Post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -65,7 +65,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all(),
+        ]);
     }
 
     /**
@@ -73,7 +76,25 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:225',
+            'category_id' => 'required',
+            'body' => 'required',
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validateData = $request->validate($rules);
+
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::where('id', $post->id)->update($validateData);
+
+        return redirect('/dashboard/posts')->with('success', 'New post has been updated');
+
     }
 
     /**
@@ -81,7 +102,10 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
+
     }
 
     public function checkSlug(Request $request)
